b7ce502f52b13491f02e98a305b95e25a2ce5203
Anastasiya Razumeyko <anastasiya.razumeyko@gmail.com>
2018-01-25 23:27:27 +0000

    added some js

--- js/main.js
+++ js/main.js
@@ -2,3 +2,14 @@ $(document).ready(function(){
 	/* calculator*/
 
 });
+
+$(document).ready(function(){
+	$(".question+div").hide();
+	$(".question").click(function(){
+		if($(this).hasClass("active")){
+			$(this).removeClass("active").next().slideDown("500");
+		}else{
+			$(this).addClass("active").next().slideUp(200);
+		}
+	});
+})
